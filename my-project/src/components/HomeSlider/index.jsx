14c2573a183057f96f1e3298b87aa9037e387789
kirti sengar <kirtisengar95@gmail.com>
2026-02-28 13:21:09 +0530

    Refine homepage banner slider UI styling

--- my-project/src/components/HomeSlider/index.jsx
+++ my-project/src/components/HomeSlider/index.jsx
@@ -7,31 +7,43 @@ import { Navigation } from 'swiper/modules';
 
 const HomeSlider = () => {
   return (
-    <div className="homeSlider">
+    <div className="homeSlider py-4">
         <div className="container">
-        <Swiper navigation={true} modules={[Navigation]} className="SliderHome">
+        <Swiper spaceBetween={10}  navigation={true} modules={[Navigation]} className="SliderHome">
         <SwiperSlide>
+        <div className="item rounded-[20px] overflow-hidden">
         <img src="https://api.spicezgold.com/download/file_1734524878924_1721277298204_banner.jpg" alt="Banner slide" className="w-full"/>
+        </div>
         </SwiperSlide>
 
         <SwiperSlide>
+        <div className="item rounded-[20px] overflow-hidden">
         <img src="https://api.spicezgold.com/download/file_1734524930884_NewProject(6).jpg" alt="Banner slide" className="w-full"/>
+        </div>
         </SwiperSlide>
 
         <SwiperSlide>
+        <div className="item rounded-[20px] overflow-hidden">
         <img src="https://api.spicezgold.com/download/file_1734524985581_NewProject(11).jpg" alt="Banner slide" className="w-full"/>
+        </div>
         </SwiperSlide>
 
         <SwiperSlide>
+        <div className="item rounded-[20px] overflow-hidden">
         <img src="https://api.spicezgold.com/download/file_1734525014348_NewProject(7).jpg" alt="Banner slide" className="w-full"/>
+        </div>
         </SwiperSlide>
 
         <SwiperSlide>
+        <div className="item rounded-[20px] overflow-hidden">
         <img src="https://api.spicezgold.com/download/file_1734524971122_NewProject(8).jpg" alt="Banner slide" className="w-full"/>
+        </div>
         </SwiperSlide>
 
         <SwiperSlide>
+        <div className="item rounded-[20px] overflow-hidden">
         <img src="https://api.spicezgold.com/download/file_1734525002307_1723967638078_slideBanner1.6bbeed1a0c8ffb494f7c.jpg" alt="Banner slide" className="w-full"/>
+        </div>
         </SwiperSlide>
         
       </Swiper>
