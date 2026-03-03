09e5c12cc64e29a69d774390c77377ffd3ebe423
kirti sengar <kirtisengar95@gmail.com>
2026-03-03 18:35:52 +0530

    Update ProductItem with hover effects and action buttons

--- my-project/src/components/ProductItem/index.jsx
+++ my-project/src/components/ProductItem/index.jsx
@@ -2,12 +2,42 @@ import React from 'react';
 import "../ProductItem/style.css";
 import {Link} from 'react-router-dom'
 import Rating from '@mui/material/Rating';
+import Button from "@mui/material/Button";
+import { FaRegHeart } from "react-icons/fa";
+import {IoGitCompareOutline} from "react-icons/io5";
+import { MdZoomOutMap } from "react-icons/md";
+
 
 const ProductItem = () => {
   return( 
     <div className="productItem shadow-lg rounded-md overflow-hidden border-1 border-[rgba(0,0,0,0.1)]">
-      <div className="imgWrapper w-full h-[220px] overflow-hidden rounded-md">
+      <div className="group imgWrapper w-full overflow-hidden rounded-md relative">
+        <Link to="/">
+        <div className="img h-[220px] overflow-hidden">
         <img src="https://api.spicezgold.com/download/file_1734529297929_fiorra-women-s-teapot-blue-pure-cotton-a-line-kurta-with-sharara-and-dupatta-product-images-rvo9n8udfg-0-202307260626.jpg" className="w-full"/>
+
+        <img src="https://api.spicezgold.com/download/file_1734529297930_fiorra-women-s-teapot-blue-pure-cotton-a-line-kurta-with-sharara-and-dupatta-product-images-rvo9n8udfg-2-202307260626.jpg" className="w-full transition-all duration-700 absolute top-0 left-0 opacity-0 group-hover:opacity-100 group-hover:scale-105"/>
+
+
+        </div>
+        </Link>
+
+        <span className="discount flex items-center absolute top-[10px] left-[10px] z-50 bg-primary text-white rounded-lg p-1 text-[12px] font-[500]">10%</span>
+
+
+
+        <div className="actions absolute top-[-200px] right-[5px] z-50 flex items-center gap-2 flex-col w-[50px] transition-all duration-300 group-hover:top-[15px] opacity-0 group-hover:opacity-100">
+
+          
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white text-black hover:!bg-primary hover:text-white group"><MdZoomOutMap className="text-[18px] !text-black group-hover:text-white hover:!text-white"/></Button>
+          
+
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white text-black hover:!bg-primary hover:text-white group"><IoGitCompareOutline className="text-[18px] text-black group-hover:text-white"/></Button>
+
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white text-black hover:!bg-primary hover:text-white group"><FaRegHeart className="text-[18px] text-black group-hover:text-white"/></Button>
+
+          
+        </div>
       </div>
 
       <div className='info p-3 py-5 '>
